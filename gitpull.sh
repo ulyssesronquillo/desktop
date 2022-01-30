21d4cd9b6b9ca41f32edde0dec72d0ac625987ef
Ulysses <ulysses@penguin>
2022-01-30 10:31:21 -0500

    added desktop/gitpull.sh

---
+++ gitpull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ulysses/Code/github/ulyssesronquillo/aws; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/desktop; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/gcloud; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/kubernetes; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/terraform; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/tfc; git pull
+cd /home/ulysses/Code/github/ulyssesronquillo/uly.me; git pull
+
